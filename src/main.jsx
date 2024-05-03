3fed3cc5881c4b9e55c926fd0076ee0576a4c885
nurhadighi24 <nurhadi.ghifari24@gmail.com>
2024-05-04 00:13:31 +0700

    install tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "../src/pages/App";
+import App from "./pages/App.jsx";
 import "../src/styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
