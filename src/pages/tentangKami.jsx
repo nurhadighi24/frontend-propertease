f0f0fad385e2dc7db5dffe4b23aa62d0ba93a95f
nurhadighi24 <nurhadi.ghifari24@gmail.com>
2024-07-25 22:49:36 +0700

    ref: refactor for style tentang kami page

--- src/pages/tentangKami.jsx
+++ src/pages/tentangKami.jsx
@@ -7,7 +7,7 @@ export default function TentangKami() {
     <>
       <Navbar />
       <div className="flex justify-center items-center">
-        <div className="bg-blue-primary w-[50rem] h-[20rem] flex flex-col justify-center items-center m-5 rounded-lg">
+        <div className="bg-blue-primary w-full h-[20rem] flex flex-col justify-center items-center m-5 rounded-lg">
           <h2 className="text-white font-bold text-6xl">PropertEase</h2>
           <p className="text-white text-xl">
             Kemudahan dalam mencari properti impian anda
@@ -15,12 +15,13 @@ export default function TentangKami() {
         </div>
       </div>
       <p className="text-blue-primary text-2xl font-bold m-5">Tentang Kami</p>
-      <p className="m-5">
-        PropertEase merupakan marketplace properti online yang menyediakan
-        pengalaman berbelanja properti yang menyenangkan dan transparan. Kami
-        bertujuan membantu Anda menemukan rumah atau investasi properti yang
-        tepat. kami menawarkan beragam pilihan properti yang dapat disesuaikan
-        dengan kebutuhan dan preferensi Anda. Kami percaya setiap langkah dalam
+      <p className="m-5 text-xl">
+        <span className="text-2xl font-semibold">PropertEase </span>
+        merupakan marketplace properti online yang menyediakan pengalaman
+        berbelanja properti yang menyenangkan dan transparan. Kami bertujuan
+        membantu Anda menemukan rumah atau investasi properti yang tepat. kami
+        menawarkan beragam pilihan properti yang dapat disesuaikan dengan
+        kebutuhan dan preferensi Anda. Kami percaya setiap langkah dalam
         perjalanan properti Anda harus memberikan kepastian, kenyamanan, dan
         kebahagiaan. Temukan properti impian Anda bersama PropertEase.
       </p>
