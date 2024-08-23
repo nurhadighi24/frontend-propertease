f3a4d08b0d12bd7d1784cd4b2115b296152b81bc
nurhadighi24 <nurhadi.ghifari24@gmail.com>
2024-08-23 13:57:26 +0700

    style: add image contact us

--- src/pages/contactUs.jsx
+++ src/pages/contactUs.jsx
@@ -85,7 +85,7 @@ export default function ContactUs() {
           </p>
         </div>
         <div>
-          <img src="../public/contactus-image.png" alt="" />
+          <img src="/contactus-image.png" alt="" />
         </div>
       </div>
 
